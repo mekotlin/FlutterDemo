026a65e50507819a1f8e84a577e600d5c2133c7d
Dnyaneshwar <dnyaneshwar@onevoicetransmedia.com>
2019-05-06 18:17:38 +0530

    Changes

---
+++ lib/screens/screen_ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginUI extends StatelessWidget{
+@override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello Flutter'),
+        ),
+      ),
+    );
+  }
+}
