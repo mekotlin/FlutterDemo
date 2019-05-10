3dfc4c7aaceaa028f243ae6d5e4160c25b4c744a
Dnyaneshwar <dnyaneshwar@onevoicetransmedia.com>
2019-05-10 16:27:43 +0530

    Login Completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,94 +2,53 @@ import 'package:flutter/material.dart';
 import 'screens/screen_ui.dart';
 import 'dart:async';
 
-void main() => runApp(MyApplication());
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+      theme: ThemeData(
+          primarySwatch: Colors.pink,
+          brightness: Brightness.light,
+          accentColor: Colors.red),
+    ));
 
-class MyApplication extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.orange),
-      home: LoginPage(),
-    );
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();  
+    loadScreen();
   }
-}
 
-class LoginPage extends StatefulWidget {
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
+  Future<Timer> loadScreen() async {
+    return Timer(Duration(seconds: 3), _loadUI);
+  }
 
-class _LoginPageState extends State<LoginPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(
-        
+  void _loadUI() async {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => LoginUI()));
+  }
+
+  Widget _showSplashScreen() {
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('images/splash.png'), fit: BoxFit.contain)),
+      child: Center(
+        child: CircularProgressIndicator(
+          valueColor: AlwaysStoppedAnimation<Color>(Colors.redAccent),
+        ),
       ),
     );
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _showSplashScreen(),
+    );
+  }
 }
-// class myApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: "MyApp",
-//       home: HomePage(),
-//       theme: ThemeData(
-//           primarySwatch: Colors.green,
-//           brightness: Brightness.light,
-//           accentColor: Colors.red),
-//     );
-//   }
-// }
-
-// class HomePage extends StatefulWidget {
-//   @override
-//   _HomePageState createState() => _HomePageState();
-// }
-
-// class _HomePageState extends State<HomePage> {
-//   String myText = "Hello World";
-//   void _changeText() {
-//     setState(() {
-//       if (myText.startsWith('H')) {
-//         myText = 'Welcome to my world';
-//       } else {
-//         myText = 'Hello World';
-//       }
-//     });
-//   }
-
-//   Widget _bodyWidget() {
-//     return Container(
-//       padding: const EdgeInsets.all(8.0),
-//       child: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             Text(
-//               myText,
-//               style: TextStyle(fontSize: 20.0),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Home Page'),
-//       ),
-//       body: _bodyWidget(),
-//       floatingActionButton: FloatingActionButton(
-//         child: Icon(Icons.add),
-//         onPressed: _changeText,
-//       ),
-//     );
-//   }
-// }
--- lib/screens/screen_ui.dart
+++ lib/screens/screen_ui.dart
@@ -1,12 +1,93 @@
 import 'package:flutter/material.dart';
 
-class LoginUI extends StatelessWidget {
+class LoginUI extends StatefulWidget {
+  @override
+  _LoginUIState createState() => _LoginUIState();
+}
+
+class _LoginUIState extends State<LoginUI> {
+  final _minimumPadding = 5.0;
+  TextEditingController userNameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  var _appKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello Flutter'),
+    return Scaffold(
+      body: Form(
+        key: _appKey,
+        child: Padding(
+          padding: EdgeInsets.only(top: _minimumPadding * 5),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                  padding: EdgeInsets.all(_minimumPadding * 5),
+                  child: Image.asset(
+                    'images/intro.png',
+                    fit: BoxFit.contain,
+                  )),
+              Padding(
+                padding: EdgeInsets.all(_minimumPadding * 2),
+                child: TextFormField(
+                  keyboardType: TextInputType.text,
+                  controller: userNameController,
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return 'Please enter user name';
+                    }
+                  },
+                  decoration: InputDecoration(
+                      labelText: 'User Name',
+                      hintText: 'Enter user name',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0))),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(_minimumPadding * 2),
+                child: TextFormField(
+                  keyboardType: TextInputType.text,
+                  controller: passwordController,
+                  obscureText: true,
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return 'Please enter password';
+                    }
+                  },
+                  decoration: InputDecoration(
+                      labelText: 'Password',
+                      hintText: 'Enter Password',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0))),
+                ),
+              ),
+              Container(
+                width: _minimumPadding * 5,
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                    bottom: _minimumPadding, top: _minimumPadding),
+                child: RaisedButton(
+                  color: Theme.of(context).primaryColor,
+                  textColor: Theme.of(context).primaryColorLight,
+                  child: Text("LOGIN"),
+                  onPressed: () {
+                    setState(() {
+                      if (_appKey.currentState.validate()) {
+                        Navigator.of(context).push(
+                            MaterialPageRoute(builder: (context) => LoginUI()));
+                      }
+                    });
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
